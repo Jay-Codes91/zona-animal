ca5046f0f8a80274ad4a309b76c739a2689aee02
Yeiko <yeimaraya91@gmail.com>
2021-06-30 20:44:05 -0600

    Agregado boostrap, Jquery, Alertify e Iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+declare var alertify: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Proyecto';
+  center = "text-center";
+  ngOnInit(){
+    /*$("h1").text("Titulo");
+    alertify.defaults.theme.ok = "btn btn-primary";
+    alertify.defaults.theme.cancel = "btn btn-danger";
+    alertify.defaults.theme.input = "form-control";*/
+  }
+
+  
 }
