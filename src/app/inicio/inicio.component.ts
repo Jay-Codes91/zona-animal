b97abf0198f360fd8bcf297fd559ed9d50f69626
Yeiko <yeimaraya91@gmail.com>
2021-07-05 22:23:47 -0600

    Cambios para agregar un carousel en el componente inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
+import { Identifiers } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { DatosAnimalesService } from '../datos-animales.service';
+declare var $: any;
 
 @Component({
   selector: 'app-inicio',
@@ -10,14 +12,16 @@ export class InicioComponent implements OnInit {
 
   mensaje: string = "";
   mamiferos: any[] = [];
+  todosAnimales: any[] = [];
 
   constructor(private servicio: DatosAnimalesService) { 
     this.mensaje = servicio.prueba;
     this.mamiferos = servicio.listaMamiferos();
-    
+    this.todosAnimales = servicio.listaTodosAnimales();
+
   }
 
-  ngOnInit(): void {
-  }
+
+  ngOnInit(): void {}
 
 }
