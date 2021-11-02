924e6e8cffbebcfdf6af48f8ffaffbb25050a572
Jay <yhgg@gmail.com>
2021-11-02 13:46:42 -0600

    Mejoras visuales en el inicio y la información de los peces es aleatoria

--- src/app/Components/inicio/inicio.component.ts
+++ src/app/Components/inicio/inicio.component.ts
@@ -1,4 +1,3 @@
-import { Identifiers } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { DatosAnimalesService } from '../../Services/datos-animales.service';
 
@@ -14,11 +13,36 @@ export class InicioComponent implements OnInit {
 
   mamiferos: any[] = [];
   todosAnimales: any[] = [];
+  //Solución temporal de los arreglos
+  animalesAleatorios: number[] = [];
+  animalesAleatorios2: number[] = [];
+  animalesAleatorios3: number[] = [];
+  animalesAleatorios4: number[] = [];
+
+  animalesCarousel: number[] = [];
+  animalesCarousel2: number[] = [];
+  animalesCarousel3: number[] = [];
+  animalesCarousel4: number[] = [];
+  animalesCarousel5: number[] = [];
+
+  min: number;
+  max: number;
 
   constructor(private servicio: DatosAnimalesService) { 
     this.mamiferos = servicio.listaMamiferos();
     this.todosAnimales = servicio.listaTodosAnimales();
+    this.min = 1;
+    this.max = this.todosAnimales.length;
+    this.animalesAleatorios.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesAleatorios2.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesAleatorios3.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesAleatorios4.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
 
+    this.animalesCarousel.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesCarousel2.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesCarousel3.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesCarousel4.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
+    this.animalesCarousel5.push(Math.floor(Math.random() * ((this.max +1)- this.min) + this.min));
   }
 
 
@@ -29,6 +53,9 @@ export class InicioComponent implements OnInit {
 	    duration: 500,
 	    easing: 'ease-in-out'
     });
+
+    
   }
+
 
 }
